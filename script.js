24d1d42df759d02757b3f40ee662b74f65a9bb11
Karelyn Rojas <knrojas@ucdavis.edu>
2023-04-23 22:15:30 -0700

    For each time block:
    1)Save buttons save respective text to local storage
    
    2)Refresh page retireves saved text

--- script.js
+++ script.js
@@ -2,114 +2,45 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var today = dayjs();   // The current date from dayjs
-  $('#currentDay').text(today.format('dddd, MMMM D')); // Replace the text of the id currentDay with formatted date
-  var currentTime24Hours = today.format('HH')  
-  console.log(currentTime24Hours);// 24 hour time of the current time
+  var today = dayjs(); // The current date from dayjs
+  $("#currentDay").text(today.format("dddd, MMMM D")); // Replace the text of the id currentDay with formatted date
+  var currentTime24Hours = today.format("HH"); //the current time in 24 hour format
+  var children = $(".container-lg").children(); //children of section class titled container-lg
 
 
-  var d = new Date("1/1/2013 " + "9:00 PM"); 
-  console.log(d.getHours());
-  // 9 -> 9
-  // 10 -> 10
-  // 11 -> 11
-  // 12 -> 12PM
-  // 13 -> 1PM
-  // 14 -> 2PM
-  // 15 -> 3PM
-  // 16 -> 4PM
-  // 17 -> 5PM
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? 
-  // How can Day.js be used to get the current hour in 24-hour time?
-  var children = $(".container-lg").children()
-  //console.log(children)
+  //For loop that goes through the children then gets the attribute that corresponds to each child by id 
+  //Each id attributes is the hour
   for (var i = 0; i < children.length; i++) {
     let child = children[i];
-    console.log(child);
-   var hour = parseInt(child.getAttribute("id").split("-")[1])
-    console.log(hour);
-    console.log(typeof hour);
-  
+    var hour = parseInt(child.getAttribute("id").split("-")[1]);
 
-    
-    // get child element
-    // convert to 24hours
-    // then compare the child's element to current time in 24 hours
-      // then add class
 
+    //classifying children by comparing the current time to the class time
     if (hour == currentTime24Hours) {
-      console.log("Present");
-      child.classList.add("present")
-    } else if(hour < currentTime24Hours){
-      console.log("Past");
-      child.classList.add("past")
-    } else if (hour > currentTime24Hours){
-      console.log("Future");
-      child.classList.add("future")
+      child.classList.add("present");
+    } else if (hour < currentTime24Hours) {
+      child.classList.add("past");
+    } else if (hour > currentTime24Hours) {
+      child.classList.add("future");
     }
-   }
- 
-  //  container-lg 
-    // go through all of its children
-    // hour-9 to hour-5
-    // compare the number with the current hour
-      // if current hour is the same -> present
-        // add the present class to the element
-      // else if number is less than current time -> past
-        // add the past class to the element
-      // else -> future
-        // add the future class to the element
+  }
 
-  
-//if the ID is equal to AND the same as the current current time  
-  //then it is present ===
-    //else
-      //it is future if greater than the current time >
-        //then highlight in green
-      //it is pass if lesser than the current time <
-        //then highlight in red
+  //saves information inputted by user into local storage for each time block
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var id = event.currentTarget.id;
+    var hour = parseInt(id.split("-")[1]);
+    var textHour = `#text-${hour}`;
+    var textAreaVal = $(textHour).val();
 
- 
+    localStorage.setItem(textHour, textAreaVal);
+  });
+
+  //For loop that checks local storage via timeblock key for saved text
+  // if there's saved text then we replace each hour's text with that saved text 
+  for (let i = 9; i < 18; i++) {
+    if (localStorage.getItem(`#text-${i}`) !== null) {
+      $(`#text-${i}`).val(localStorage.getItem(`#text-${i}`));
+    }
+  }
 });
-
-
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-rendertextarea();
-  var textarea = localStorage.getItem("textarea"
-  localStorage.setItem("textarea",textarea);
-  
-  
-  const myValue = localStorage.getItem('textarea');
-  //prevent default
-  //localStorage.removeItem('myKEy');
-  //localStorage.clear();
-
-  
-
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-
-  
-  //addButton.addEventListener("click",function(){
-    //if (){
-
-
-    //  localStorage.setItem();
-  //  }
-  //});
